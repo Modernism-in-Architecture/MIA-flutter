03211902bf13114ce04e30f54bbc64b914cfb813
Norma Driske <norma@entwicklerheld.de>
2022-09-10 01:25:49 +0200

    Add detail API client

--- lib/src/network/api_client.dart
+++ lib/src/network/api_client.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'package:mia/src/models/detail_building_model.dart';
 import 'package:mia/src/network/constants.dart';
 import 'package:mia/src/models/list_building_model.dart';
 
@@ -24,7 +25,7 @@ class ApiService {
           headers: _header,
       );
       if (response.statusCode == 200) {
-        log("API CALL MADE");
+        log("BUILDING LIST API CALL MADE");
         var jsonResponse = json.decode(response.body);
         List<dynamic> data = jsonResponse["data"];
         return data.map((buildings) => ListBuildingModel.fromJson(buildings)).toList();
@@ -33,6 +34,25 @@ class ApiService {
       log(e.toString());
     }
     return listBuildings;
+  }
+
+  Future<DetailBuildingModel> getBuildingDetails(int buildingId) async {
+    late Future<DetailBuildingModel> building = {} as Future<DetailBuildingModel>;
+    try {
+      var url = Uri.parse("${ApiConstants.baseUrl}${ApiConstants.apiVersionPath}${ApiConstants.buildingListEndpoint}$buildingId/");
+      var response = await http.get(
+        url,
+        headers: _header,
+      );
+      if (response.statusCode == 200) {
+        log("DETAIL API CALL MADE");
+        Map<String, dynamic> buildingData = jsonDecode(response.body)["data"] as Map<String, dynamic>;
+       return DetailBuildingModel.fromJson(buildingData);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return building;
   }
 }
 
