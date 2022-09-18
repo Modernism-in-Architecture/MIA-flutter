4cb7acf5a18110f929f02c316de13139e1f6fd66
Norma Driske <norma@entwicklerheld.de>
2022-09-19 01:30:01 +0200

    Add ArchitectsListView

--- lib/src/views/architects_list_view.dart
+++ lib/src/views/architects_list_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mia/main.dart';
+
+import '../widgets/loading_screen.dart';
+import 'architect_detail_view.dart';
 
 class ArchitectsListView extends ConsumerStatefulWidget {
   const ArchitectsListView({Key? key}) : super(key: key);
@@ -9,15 +13,47 @@ class ArchitectsListView extends ConsumerStatefulWidget {
 }
 
 class ArchitectsListViewState extends ConsumerState<ArchitectsListView> {
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
-        Center(
-          child: Text("This the Architects Screen", style: TextStyle(fontSize: 24),),
-        )
-      ],
+
+    final listArchitects = ref.watch(architectsListDataProvider);
+
+    return listArchitects.when(
+        data: (listArchitects) {
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: listArchitects.length,
+              itemBuilder: (BuildContext context, int index){
+                return Container(
+                    margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    padding: const EdgeInsets.all(10.0),
+                    child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ArchitectDetailView(architectId: listArchitects[index].id),
+                            ),
+                          );
+                        },
+                        child: Column (
+                            children: [
+                              Row(
+                                children: [
+                                  Text(listArchitects[index].lastName, style: const TextStyle(fontSize: 16)),
+                                  if (listArchitects[index].firstName != "") const Text(", "),
+                                  Text(listArchitects[index].firstName, style: const TextStyle(fontSize: 16))
+                                ]
+                              )
+                            ]
+                        )
+                    ));
+              },
+            );
+      },
+      error: (err, s) => Text(err.toString()),
+      loading: () => const LoadingScreen(),
     );
   }
 }
