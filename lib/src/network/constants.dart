56ed2ca0fa8350759f5c10210e0dd6255daf524f
Norma Driske <norma@entwicklerheld.de>
2022-09-19 01:26:17 +0200

    Add architectListEndpoint

--- lib/src/network/constants.dart
+++ lib/src/network/constants.dart
@@ -2,4 +2,5 @@ class ApiConstants {
   static String baseUrl = 'https://modernism-in-architecture.org';
   static String apiVersionPath = '/api/v1';
   static String buildingListEndpoint = '/buildings/';
+  static String architectListEndpoint = '/architects/';
 }
