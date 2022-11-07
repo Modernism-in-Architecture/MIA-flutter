f34f2e71150722d9fe5ee8cb4b8e039956859c90
Norma Driske <norma@entwicklerheld.de>
2022-11-07 10:36:04 +0100

    Extract map marker to widgets

---
+++ lib/src/widgets/maps/map_marker.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../views/building_detail_view.dart';
+
+
+class MapMarker extends StatefulWidget{
+  const MapMarker({Key? key, required this.buildingId}) : super(key: key);
+  final int buildingId;
+
+  @override
+  State<MapMarker> createState() => MapMarkerState();
+}
+
+
+class MapMarkerState extends State<MapMarker> {
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Icon(
+        Icons.location_on,
+        color: Colors.blue[800],
+        size: 42.0,
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BuildingDetailView(buildingId: widget.buildingId),
+          ),
+        );
+      },
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
