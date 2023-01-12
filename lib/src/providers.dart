bf24c1986f69b46283799dd6ef65dc4707b75817
Norma Driske <norma@entwicklerheld.de>
2023-01-13 00:42:50 +0100

    Add bookmark provider

--- lib/src/providers.dart
+++ lib/src/providers.dart
@@ -1,18 +1,72 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mia/src/widgets/custom_title_bar.dart';
 
 import 'models/list_architect_model.dart';
 import 'models/list_building_model.dart';
 import 'network/mia_api_client.dart';
+import 'package:path_provider/path_provider.dart';
 
-List titles = ["Buildings", "Places", "Architects"];
+List titles = ["Buildings", "Places", "Architects", "Bookmarks"];
 
 class MapLocation {
   MapLocation({required this.longitude, required this.latitude});
-
   double? latitude;
   double? longitude;
+}
+
+Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+  return directory.path;
+}
+
+Future<File> get _localFile async {
+  final path = await _localPath;
+  return File('$path/bookmarks.json');
+}
+
+class Bookmarks<StateProvider> {
+  List<int> _bookmarks = [];
+  List<int> get bookmarks => _bookmarks;
+
+  Future<void> loadBookmarks() async {
+    try {
+      final file = await _localFile;
+      String jsonString = await file.readAsString();
+      List<dynamic> jsonData = jsonDecode(jsonString);
+      _bookmarks = jsonData.whereType<int>().map((e) => e).toList();
+      print(_bookmarks);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> saveBookmarks() async {
+    try {
+      final file = await _localFile;
+      String jsonString = jsonEncode(_bookmarks);
+      await file.writeAsString(jsonString);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  void addBookmark(int bookmark) {
+    _bookmarks.add(bookmark);
+    saveBookmarks();
+  }
+
+  void removeBookmark(int bookmark) {
+    _bookmarks.remove(bookmark);
+    saveBookmarks();
+  }
 }
 
 final locationPermissionGrantedByUser = StateProvider<bool>((ref) {
@@ -64,4 +118,8 @@ final buildingsListDataProvider = FutureProvider<List<ListBuildingModel>>((ref) 
 
 final architectsListDataProvider = FutureProvider<List<ListArchitectModel>>((ref) async {
   return ref.read(miaApiProvider).getArchitects();
+});
+
+final bookmarksProvider = StateProvider<Bookmarks>((ref) {
+  return Bookmarks();
 });
